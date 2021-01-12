e527cc0fa4a0bbfc9a57f3e1b0bcc21d1d9f000a
Jeff Li <jli40@mit.edu>
2021-01-11 19:52:13 -0500

    Progress on landing page and test page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { Router } from "@reach/router";
 import NotFound from "./pages/NotFound.js";
 import Skeleton from "./pages/Skeleton.js";
+import Landing from "./pages/Landing.js";
+import Test from "./pages/Test.js";
 
 import "../utilities.css";
 
@@ -45,11 +47,29 @@ class App extends Component {
   };
 
   render() {
+    if( this.state.userId === undefined) {
+      return (
+        <>
+          <Landing
+            path="/"
+            handleLogin={this.handleLogin}
+            handleLogout={this.handleLogout}
+            userId={this.state.userId}
+          />
+        </>
+      );
+    }
     return (
       <>
         <Router>
-          <Skeleton
+          <Test
             path="/"
+            handleLogin={this.handleLogin}
+            handleLogout={this.handleLogout}
+            userId={this.state.userId}
+          />
+          <Skeleton
+            path="/skeleton"
             handleLogin={this.handleLogin}
             handleLogout={this.handleLogout}
             userId={this.state.userId}
---
+++ client/src/components/pages/Test.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import GoogleLogin, { GoogleLogout } from "react-google-login";
+
+import "../../utilities.css";
+
+//TODO: REPLACE WITH YOUR OWN CLIENT_ID
+const GOOGLE_CLIENT_ID = "121479668229-t5j82jrbi9oejh7c8avada226s75bopn.apps.googleusercontent.com";
+
+class Test extends Component {
+  constructor(props) {
+    super(props);
+    // Initialize Default State
+    this.state = {};
+  }
+
+  componentDidMount() {
+    // remember -- api calls go here!
+  }
+
+  render() {
+    return (
+      <>
+        {this.props.userId ? (
+          <GoogleLogout
+            clientId={GOOGLE_CLIENT_ID}
+            buttonText="Logout"
+            onLogoutSuccess={this.props.handleLogout}
+            onFailure={(err) => console.log(err)}
+          />
+        ) : (
+          <GoogleLogin
+            clientId={GOOGLE_CLIENT_ID}
+            buttonText="Login"
+            onSuccess={this.props.handleLogin}
+            onFailure={(err) => console.log(err)}
+          />
+        )}
+        <div>You have reached the test page.</div>
+      </>
+    );
+  }
+}
+
+export default Test;
