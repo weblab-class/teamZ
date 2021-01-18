068fa8f750c4ae2e20163d5d0be7d126bb88cefb
Jeff Li <jli40@mit.edu>
2021-01-17 23:52:20 -0500

    fixed nasty bug

--- client/src/components/modules/SidePane.js
+++ client/src/components/modules/SidePane.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 import "../../utilities.css";
 
+const tileButtonSize = 64;
 class SidePane extends Component {
   constructor(props) {
     super(props);
@@ -11,27 +12,79 @@ class SidePane extends Component {
     // callbacks:
     //  -  setCurrentTile
     //  -  displayTileDesigner
-    this.state = {};
+    const refDict = {};
+    Object.keys(props.tiles).forEach((tileId) => {
+      refDict[tileId] = React.createRef();
+    });
+    this.state = {
+      layer: "Platform",
+      refDict: refDict,
+    };
+  }
+
+  componentDidMount() {
+    Object.keys(this.state.refDict).forEach((tileId) => {
+      const canvas = this.refDict[tileId].current;
+      const context = canvas.getContext("2d");
+      const im = this.props.tiles[tileId].image;
+      context.drawImage(im, 0, 0, tileButtonSize, tileButtonSize);
+    });
+  }
+
+  componentDidUpdate(prevProps) {
+    if (Object.keys(prevProps.tiles).length !== Object.keys(this.props.tiles).length) {
+      const refDict = {};
+      Object.keys(this.state.refDict).forEach((tileId) => {
+        refDict[tileId] = this.state.refDict[tileId];
+      });
+      Object.keys(this.props.tiles).forEach((tileId) => {
+        refDict[tileId] = React.createRef();
+      });
+      this.setState({ refDict: refDict }, () => {
+        Object.keys(this.state.refDict).forEach((tileId) => {
+          const canvas = this.state.refDict[tileId].current;
+          const context = canvas.getContext("2d");
+          const im = this.props.tiles[tileId].image;
+          context.drawImage(im, 0, 0, tileButtonSize, tileButtonSize);
+        });
+      });
+    }
   }
 
   render() {
-    const tileButtons = Object.keys(this.props.tiles).map((tileId) => {
-      return (
-        <li key={tileId}>
-          <button
+    const tileButtons = Object.keys(this.props.tiles)
+      .filter((tileId) => {
+        return this.props.tiles[tileId].layer === this.state.layer;
+      })
+      .map((tileId) => {
+        return (
+          <div
+            className="tileButton"
+            key={tileId}
             onClick={(e) => {
               this.props.setCurrentTile(tileId);
             }}
           >
-            {`Tile: ${this.props.tiles[tileId].name}`}
-          </button>
-        </li>
-      );
-    });
+            {this.props.tiles[tileId].name}
+            <canvas
+              width={tileButtonSize}
+              height={tileButtonSize}
+              ref={this.state.refDict[tileId]}
+            />
+          </div>
+        );
+      });
     return (
       <div className="u-flexColumn">
-        <div>Side pane</div>
-        <ul>{tileButtons}</ul>
+        tile select
+        <div
+          onClick={(e) => {
+            this.setState({ layer: this.state.layer === "Platform" ? "Background" : "Platform" });
+          }}
+        >
+          {this.state.layer}
+        </div>
+        <div>{tileButtons}</div>
         <div>
           Current tile:{" "}
           {this.props.currentTile in this.props.tiles
