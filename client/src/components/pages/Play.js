2946c2b802674b2ee3d14205fd6117b41a5d5a3f
Jeff Li <jli40@mit.edu>
2021-01-18 19:00:47 -0500

    progress on game logic

---
+++ client/src/components/pages/Play.js
@@ -0,0 +1,230 @@
+import React, { Component } from "react";
+import GoogleLogin, { GoogleLogout } from "react-google-login";
+import { get, post } from "../../utilities.js";
+import { socket } from "../../client-socket";
+// import { drawEditCanvas } from "../../editCanvasManager";
+// import { initInput } from "../../editInput.js";
+import { Link } from "@reach/router";
+
+import "../../utilities.css";
+import "./Play.css";
+
+import { tileSize, tileSizeOnCanvas } from "../../../../constants";
+class Play extends Component {
+  constructor(props) {
+    super(props);
+    this.canvasRef = React.createRef();
+    this.canvasWidth = 900;
+    this.canvasHeight = 700;
+    const emptyFn = () => {
+      console.log("empty fn");
+    };
+    // for now, the only prop the Edit page should take is :levelId
+    // Initialize Default State
+    this.state = {
+      tiles: {}, // maps tileId to tile object, including actual images
+      //            tile object has name, layer, image attributes
+      fetching: {}, // tileIds -> true
+      currentTile: "no current tile", // tileId of currentTile
+      tempNewTileName: "",
+      tempNewTileLayer: "Platform",
+      tempNewTileR: 128,
+      tempNewTileG: 128,
+      tempNewTileB: 128,
+      clearInputFn: emptyFn,
+    };
+  }
+
+  componentDidMount() {
+    // api calls here
+    post("/api/joinLevel", {
+      levelId: this.props.levelId,
+      canvasWidth: this.canvasWidth,
+      canvasHeight: this.canvasHeight,
+    }).then((garbage) => {
+      const clearInputFn = initInput({ canvas: this.getCanvas() });
+      this.setState({ clearInputFn: clearInputFn });
+    });
+    socket.on("update", (update) => {
+      // console.log(`mouseX: ${update.mouseX}, mouseY: ${update.mouseY}`);
+      this.processUpdate(update);
+    });
+  }
+
+  componentWillUnmount() {
+    this.state.clearInputFn();
+  }
+
+  getCanvas = () => {
+    return this.canvasRef.current;
+  };
+
+  processUpdate = async (update) => {
+    if (this.getCanvas() === null) return; //do nothing if no canvas
+    //before drawing, check if update includes any new tiles.
+    const updateAvailableTiles = update.availableTiles;
+    const tilesToFetch = [];
+    const fetchingDict = {};
+    for (let i = 0; i < updateAvailableTiles.length; i++) {
+      const tileId = updateAvailableTiles[i];
+      if (!(tileId in this.state.tiles) && !(tileId in this.state.fetching)) {
+        tilesToFetch.push(tileId);
+        fetchingDict[tileId] = true;
+      }
+    }
+    // if (tilesToFetch.length > 0) {
+    //   console.log("tilesToFetch: " + tilesToFetch);
+    //   console.log("tilesToFetchLength: " + tilesToFetch.length);
+    //   console.log("first elem of tilesToFetch: " + tilesToFetch[0]);
+    // }
+    if (tilesToFetch.length > 0) {
+      await this.setState((prevState) => {
+        return {
+          fetching: Object.assign({}, prevState.fetching, fetchingDict),
+        };
+      });
+      // Object.keys(this.state.fetching).forEach((key) => {
+      //   console.log("a key in fetching: " + key);
+      // });
+      post("/api/tilesWithId", { tileIds: tilesToFetch }).then(async (tileDict) => {
+        // console.log("received tileDict from tilesWithID call");
+        const newTiles = {};
+        await Object.keys(tileDict).forEach((tileId) => {
+          // console.log("one key in loop: " + tileId);
+          const tileObject = tileDict[tileId];
+          const imArray = tileObject.image;
+          const imArrayClamped = new Uint8ClampedArray(imArray.length);
+          for (let i = 0; i < imArray.length; i++) {
+            imArrayClamped[i] = imArray[i];
+          }
+          const tileImageData = new ImageData(imArrayClamped, tileObject.width, tileObject.height);
+          createImageBitmap(tileImageData).then((bitmap) => {
+            // console.log("created butmap successfully: " + bitmap);
+            newTiles[tileId] = {
+              _id: tileObject._id,
+              name: tileObject.name,
+              layer: tileObject.layer,
+              image: bitmap,
+            };
+          });
+        });
+        // console.log("newTiles len: " + Object.keys(newTiles).length);
+        await this.setState((prevState) => {
+          return { tiles: Object.assign({}, prevState.tiles, newTiles) };
+        });
+        // console.log("new state tiles len: " + Object.keys(this.state.tiles).length);
+        // if (Object.keys(this.state.tiles).length > 0) {
+        //   console.log("state tiles: " + this.state.tiles);
+        // }
+      });
+    }
+    if (update.currentTile !== this.state.currentTile) {
+      this.setState({ currentTile: update.currentTile });
+    }
+    drawEditCanvas(this.getCanvas(), update, this.state.tiles);
+  };
+
+  /**
+   * @param image has to be an actual image ||| FOR NOW image is just array
+   */
+  createTile = (tileName, layer, image) => {
+    // console.log(`TileName: ${tileName}, layer: ${layer}, image: ${image}`);
+    post("/api/newTile", {
+      name: tileName,
+      layer: layer,
+      image: image,
+    }).then((tileId) => {
+      // consider adding this new tile to state locally, without relying on api call
+      // console.log("created tile with id: " + tileId);
+      addTile(tileId);
+    });
+  };
+
+  render() {
+    return (
+      <div className="u-flexRow">
+        <div className="u-flexColumn">
+          <div>
+            <Link
+              to={"/"}
+              onClick={(e) => {
+                post("/api/save");
+              }}
+            >
+              Go Back
+            </Link>
+            <button
+              type="submit"
+              onClick={(e) => {
+                post("/api/save");
+              }}
+            >
+              save level
+            </button>
+          </div>
+          <canvas ref={this.canvasRef} width={this.canvasWidth} height={this.canvasHeight} />
+        </div>
+        <SidePane
+          tiles={this.state.tiles}
+          currentTile={this.state.currentTile}
+          setCurrentTile={(tileId) => {
+            changeTile(tileId);
+          }}
+          displayTileDesigner={() => {
+            /* TODO */
+          }}
+        />
+        <div>
+          Temporary tile creator
+          <textarea
+            type="text"
+            placeholder="TileName"
+            value={this.state.tempNewTileName}
+            onChange={(e) => this.setState({ tempNewTileName: e.target.value })}
+          ></textarea>
+          <textarea
+            type="text"
+            placeholder="TileLayer"
+            value={this.state.tempNewTileLayer}
+            onChange={(e) => this.setState({ tempNewTileLayer: e.target.value })}
+          ></textarea>
+          <textarea
+            type="text"
+            placeholder="TileR"
+            value={this.state.tempNewTileR}
+            onChange={(e) => this.setState({ tempNewTileR: parseInt(e.target.value) })}
+          ></textarea>
+          <textarea
+            type="text"
+            placeholder="TileG"
+            value={this.state.tempNewTileG}
+            onChange={(e) => this.setState({ tempNewTileG: parseInt(e.target.value) })}
+          ></textarea>
+          <textarea
+            type="text"
+            placeholder="TileB"
+            value={this.state.tempNewTileB}
+            onChange={(e) => this.setState({ tempNewTileB: parseInt(e.target.value) })}
+          ></textarea>
+          <button
+            type="submit"
+            onClick={(e) => {
+              const arr = [];
+              for (let i = 0; i < 4 * tileSize * tileSize; i += 4) {
+                arr.push(this.state.tempNewTileR);
+                arr.push(this.state.tempNewTileG);
+                arr.push(this.state.tempNewTileB);
+                arr.push(255);
+              }
+              this.createTile(this.state.tempNewTileName, this.state.tempNewTileLayer, arr);
+            }}
+          >
+            Add tile
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Play;
---
+++ server/playLogic.js
@@ -0,0 +1,425 @@
+const constants = require("../constants.js");
+const tileSize = constants.tileSize;
+const tileSizeOnCanvas = constants.tileSizeOnCanvas;
+const gravity = 1;
+const maxSpeed = tileSize;
+const jumpSpeed = 10;
+const walkAccel = 5;
+const airAccel = 2;
+
+// keys used in level-editor; initialize keys to not-pressed-down
+const keys = ["w", "a", "s", "d"];
+
+// state of level editor
+const playState = {
+  // ........
+  // non-player-specific information:
+  levels: {}, // maps levelId to level information consisting of
+  // rows: 0,
+  // cols: 0,
+  // gridTiles: [], // row-major array of dictionaries of the form:
+  //                // {_id: , layer: "..."}
+  // availableTiles: [], // list of available tileIds
+  // startX: 0, // abstract cors of start position
+  // startY: 0,
+  // ........
+  // player-specific information:
+  players: {},
+  // players is dict mapping id of user, containing...
+  /* { levelId -- id of level player is currently editing
+       camX
+       camY -- coordinates of top left corner of camera of player
+       x -- cor of player char
+       y 
+       xspeed -- speed of player char in x dir
+       yspeed 
+       isRestarting: Boolean
+       restartTimer: number
+       isFacingRight: Boolean
+       canvasWidth
+       canvasHeight -- number of pixels vertically in canvas
+       keyDownMap -- dictionary mapping a keyboard key to boolean -- is player holding down
+                                                         that key at the moment?
+     }
+   */
+};
+
+// ... functions called by events (not once per frame) ...
+
+/**
+ * @param key one of the strings in keys
+ */
+const registerKeyDown = (playerId, key) => {
+  if (!(playerId in playState.players)) return;
+  playState.players[playerId].keyDownMap[key] = true;
+};
+
+const registerKeyUp = (playerId, key) => {
+  if (!(playerId in playState.players)) return;
+  playState.players[playerId].keyDownMap[key] = false;
+};
+
+/**
+ * Adds a new player to a level. If player already has a level, will reset.
+ * @param {*} playerId
+ * @param {*} level dictionary containing level info
+ * @param {*} canvasWidth how many pixels across on player's canvas
+ * @param {*} canvasHeight
+ */
+const addPlayer = (playerId, level, canvasWidth, canvasHeight) => {
+  // console.log("addPlayer is called in editLogic");
+  const keyDownMap = {};
+  for (let i = 0; i < keys.length; i++) {
+    keyDownMap[keys[i]] = false;
+  }
+  const levelId = level._id;
+  playState.levels[levelId] = level; // add level no matter what
+  playState.players[playerId] = {
+    levelId: levelId,
+    camX: 0,
+    camY: 0,
+    x: level.startX,
+    y: level.startY,
+    xspeed: 0,
+    yspeed: 0,
+    isRestarting: false,
+    restartTimer: 0,
+    keyDownMap: keyDownMap,
+    canvasWidth: canvasWidth,
+    canvasHeight: canvasHeight,
+  };
+};
+
+/** Remove a player from the game state if they DC */
+const removePlayer = (playerId) => {
+  if (playerId in playState.players) {
+    delete playState.players[playerId];
+  }
+};
+
+const restartPlayer = (playerId) => {
+  if (!(playerId in playState.players)) return;
+  playState.players[playerId].isRestarting = true;
+};
+
+const toAbstractCors = (canX, canY, camX, camY) => {
+  const canvasToAbstractRatio = Math.floor(tileSizeOnCanvas / tileSize);
+  return {
+    x: Math.floor(canX / canvasToAbstractRatio + camX),
+    y: Math.floor(canY / canvasToAbstractRatio + camY),
+  };
+};
+
+// ... helper functions for update ...
+
+/**
+ * Returns whether specified cors collide with platform
+ * @param {*} levelId
+ * @param {*} absX
+ * @param {*} absY
+ */
+const hitTest = (levelId, absX, absY) => {
+  const level = playState.levels[levelId];
+  // if either cor is negative, return false
+  if (absX < 0 || absY < 0) return false;
+  // find the tile row and col
+  const col = Math.floor(absX / tileSize);
+  const row = Math.floor(absY / tileSize);
+  // if either col or row are out of range of level's size, return false
+  if (col >= level.cols || row >= level.rows) return false;
+  // now we know row and col are legit.
+  const tile = level.gridTiles[row * level.cols + col];
+  return tile.layer === "Platform";
+};
+
+const diamondFromTopLeft = (x, y) => {
+  const ret = {};
+  ret.up = { x: x + tileSize / 2, y: y };
+  ret.down = { x: x + tileSize / 2, y: y + tileSize - 1 };
+  ret.right = { x: x + tileSize - 1, y: y + tileSize / 2 };
+  ret.left = { x: x, y: y + tileSize / 2 };
+  return ret;
+};
+/**
+ * returns dict describing player cors
+ * @param {*} playerId
+ */
+const playerCors = (playerId) => {
+  const player = playState.players[playerId];
+  return diamondFromTopLeft(player.x, player.y);
+};
+
+/**
+ * return whether player is on ground
+ * @param {*} playerId
+ */
+const onGround = (playerId) => {
+  const player = playState.players[playerId];
+  const pcors = playerCors(playerId);
+  return hitTest(player.levelId, pcors.down.x, pcors.down.y + 1);
+};
+
+const clipCharCors = (playerId) => {
+  const player = playState.players[playerId];
+  const level = playState.levels[player.levelId];
+  player.x = Math.max(0, player.x);
+  player.y = Math.max(0, player.y);
+  player.x = Math.min(level.cols * tileSize - tileSize, player.x);
+  player.y = Math.min(level.rows * tileSize - tileSize, player.y);
+};
+
+const updatePlayerPosition = (playerId) => {
+  if (!(playerId in playState.players)) return;
+  const player = playState.players[playerId];
+  // first, if player is stuck in platform, move them out, and reset their speed
+  while (hitTest(player.levelId, playerCors(playerId).down.x, playerCors(playerId).down.y)) {
+    player.y--;
+    player.yspeed = 0;
+  }
+  while (hitTest(player.levelId, playerCors(playerId).up.x, playerCors(playerId).up.y)) {
+    player.y++;
+    player.yspeed = 0;
+  }
+  while (hitTest(player.levelId, playerCors(playerId).left.x, playerCors(playerId).left.y)) {
+    player.x++;
+    player.xspeed = 0;
+  }
+  while (hitTest(player.levelId, playerCors(playerId).right.x, playerCors(playerId).right.y)) {
+    player.x--;
+    player.xspeed = 0;
+  }
+  // next, check if the player's next position will collide with platform
+  // if so, reset their speed, and move them to the edge of the platform
+  let playerNextPos = { x: player.x + player.xspeed, y: player.y + player.yspeed };
+  if (
+    hitTest(
+      player.levelId,
+      diamondFromTopLeft(playerNextPos.x, playerNextPos.y).down.x,
+      diamondFromTopLeft(playerNextPos.x, playerNextPos.y).down.y
+    )
+  ) {
+    player.y += player.yspeed;
+    player.yspeed = 0;
+    while (hitTest(player.levelId, playerCors(playerId).down.x, playerCors(playerId).down.y)) {
+      player.y--;
+    }
+  }
+  playerNextPos = { x: player.x + player.xspeed, y: player.y + player.yspeed };
+  if (
+    hitTest(
+      player.levelId,
+      diamondFromTopLeft(playerNextPos.x, playerNextPos.y).up.x,
+      diamondFromTopLeft(playerNextPos.x, playerNextPos.y).up.y
+    )
+  ) {
+    player.y += player.yspeed;
+    player.yspeed = 0;
+    while (hitTest(player.levelId, playerCors(playerId).up.x, playerCors(playerId).up.y)) {
+      player.y++;
+    }
+  }
+  playerNextPos = { x: player.x + player.xspeed, y: player.y + player.yspeed };
+  if (
+    hitTest(
+      player.levelId,
+      diamondFromTopLeft(playerNextPos.x, playerNextPos.y).left.x,
+      diamondFromTopLeft(playerNextPos.x, playerNextPos.y).left.y
+    )
+  ) {
+    player.x += player.xspeed;
+    player.xspeed = 0;
+    while (hitTest(player.levelId, playerCors(playerId).left.x, playerCors(playerId).left.y)) {
+      player.x++;
+    }
+  }
+  playerNextPos = { x: player.x + player.xspeed, y: player.y + player.yspeed };
+  if (
+    hitTest(
+      player.levelId,
+      diamondFromTopLeft(playerNextPos.x, playerNextPos.y).right.x,
+      diamondFromTopLeft(playerNextPos.x, playerNextPos.y).right.y
+    )
+  ) {
+    player.x += player.xspeed;
+    player.xspeed = 0;
+    while (hitTest(player.levelId, playerCors(playerId).right.x, playerCors(playerId).right.y)) {
+      player.x--;
+    }
+  }
+
+  // // now, update the player's position based on their speed
+  player.x += player.xspeed;
+  player.y += player.yspeed;
+  // finally, update the player's speed based on gravity, keypresses, etc
+  if (onGround(playerId)) {
+    if (player.keyDownMap["w"]) {
+      // player is jumping
+      player.yspeed = -1 * jumpSpeed;
+    }
+    if (player.keyDownMap["a"]) {
+      // player is moving left
+      player.xspeed -= walkAccel;
+    }
+    if (player.keyDownMap["d"]) {
+      // player is moving right
+      player.xspeed += walkAccel;
+    }
+  } else {
+    if (player.keyDownMap["a"]) {
+      // player is moving left
+      player.xspeed -= airAccel;
+    }
+    if (player.keyDownMap["d"]) {
+      // player is moving right
+      player.xspeed += airAccel;
+    }
+  }
+  // // update gravity
+  player.yspeed += gravity;
+  // // clip speed to max speeds.
+  player.xspeed = Math.min(player.xspeed, maxSpeed);
+  player.xspeed = Math.max(player.xspeed, -1 * maxSpeed);
+  player.yspeed = Math.min(player.yspeed, maxSpeed);
+  player.yspeed = Math.max(player.yspeed, -1 * maxSpeed);
+  // clip character position.
+  clipCharCors(playerId);
+};
+
+const updatePlayerPositions = () => {
+  Object.keys(playState.players).forEach((playerId) => {
+    updatePlayerPosition(playerId);
+  });
+};
+
+/**
+ * mutates player camera to be good.
+ */
+const clipCamera = (playerId) => {
+  const player = playState.players[playerId];
+  const level = playState.levels[player.levelId];
+  const canvasToAbstractRatio = Math.floor(tileSizeOnCanvas / tileSize);
+  player.camX = Math.min(
+    player.camX,
+    Math.floor(level.cols * tileSize - player.canvasWidth / canvasToAbstractRatio)
+  );
+  player.camY = Math.min(
+    player.camY,
+    Math.floor(level.rows * tileSize - player.canvasHeight / canvasToAbstractRatio)
+  );
+  player.camX = Math.max(0, player.camX);
+  player.camY = Math.max(0, player.camY);
+};
+
+/**
+ * centers player camera on char
+ * @param {*} playerId
+ */
+const centerCamera = (playerId) => {
+  const player = playState.players[playerId];
+  const canvasToAbstractRatio = Math.floor(tileSizeOnCanvas / tileSize);
+  const cameraAbstractWidth = Math.floor(player.canvasWidth / canvasToAbstractRatio);
+  const cameraAbstractHeight = Math.floor(player.canvasHeight / canvasToAbstractRatio);
+  const charCenterX = player.x + tileSize / 2;
+  const charCenterY = player.y + tileSize / 2;
+  player.camX = Math.floor(charCenterX - cameraAbstractWidth / 2);
+  player.camY = Math.floor(charCenterY - cameraAbstractHeight / 2);
+};
+
+// update players' camera coordinates
+const updateCameras = () => {
+  Object.keys(playState.players).forEach((playerId) => {
+    centerCamera(playerId);
+    clipCamera(playerId);
+  });
+};
+
+const corsInGrid = (x, y, levelId) => {
+  return (
+    x >= 0 &&
+    y >= 0 &&
+    x < playState.levels[levelId].cols * tileSize &&
+    y < playState.levels[levelId].rows * tileSize
+  );
+};
+
+// ... update ...
+
+/**
+ * called each frame. updates the game state
+ */
+const update = () => {
+  updatePlayerPositions();
+  updateCameras();
+};
+
+const getSlice = (playerId) => {
+  const player = playState.players[playerId];
+  const level = playState.levels[player.levelId];
+  // worry about edge cases below
+  const rowStart = Math.min(Math.max(0, Math.floor(player.camY / tileSize)), level.rows);
+  const colStart = Math.min(Math.max(0, Math.floor(player.camX / tileSize)), level.cols);
+  const tilesHigh = Math.min(
+    level.rows - rowStart,
+    Math.floor(player.canvasHeight / tileSizeOnCanvas) + 2
+  );
+  const tilesWide = Math.min(
+    level.cols - colStart,
+    Math.floor(player.canvasWidth / tileSizeOnCanvas) + 2
+  );
+  const slice = [];
+  for (let i = rowStart; i < rowStart + tilesHigh; i++) {
+    for (let j = colStart; j < colStart + tilesWide; j++) {
+      slice.push(level.gridTiles[i * level.cols + j]._id);
+    }
+  }
+  return {
+    sliceRowStart: rowStart,
+    sliceColStart: colStart,
+    sliceRows: tilesHigh,
+    sliceCols: tilesWide,
+    slice: slice,
+  };
+};
+
+/**
+ * Returns instructions for player to render stuff at client side.
+ * @param {*} playerId
+ */
+const instructionsForPlayer = (playerId) => {
+  const player = playState.players[playerId];
+  const level = playState.levels[player.levelId];
+  const sliceDict = getSlice(playerId);
+  const ret = {
+    availableTiles: level.availableTiles,
+    camX: player.camX,
+    camY: player.camY,
+    sliceRowStart: sliceDict.sliceRowStart,
+    sliceColStart: sliceDict.sliceColStart,
+    sliceRows: sliceDict.sliceRows,
+    sliceCols: sliceDict.sliceCols,
+    slice: sliceDict.slice,
+    x: player.x,
+    y: player.y,
+  };
+  return ret;
+};
+
+const getInstructions = () => {
+  const ret = {};
+  Object.keys(playState.players).forEach((key) => {
+    ret[key] = instructionsForPlayer(key);
+  });
+  return ret;
+};
+
+module.exports = {
+  playState,
+  instructionsForPlayer,
+  getInstructions,
+  registerKeyDown,
+  registerKeyUp,
+  addPlayer,
+  removePlayer,
+  restartPlayer,
+  update,
+};
